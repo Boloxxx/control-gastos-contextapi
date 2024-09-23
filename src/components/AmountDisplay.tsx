ba119af82d1dcb8807f54f7c2967dd56f9e7b925
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-09-23 14:34:52 -0400

    first-commit

---
+++ src/components/AmountDisplay.tsx
@@ -0,0 +1,17 @@
+import { formarCurrency } from "../helpers";
+
+type AmountDisplayProps = {
+  label: string;
+  amount: number;
+};
+
+export default function AmountDisplay({ label, amount }: AmountDisplayProps) {
+  return (
+    <div>
+      <p className="text-2xl text-blue-600 font-bold">
+        {label}: {""}
+        <span className="font-black text-black"> {formarCurrency(amount)}</span>
+      </p>
+    </div>
+  );
+}
